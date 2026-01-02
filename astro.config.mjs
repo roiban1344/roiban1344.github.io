1bde45b9d0c321ac5287475f9529b8f87a7791a2
roiban1344 <81000453+roiban1344@users.noreply.github.com>
2026-01-02 14:52:43 +0900

    Install Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://roiban1344.github.io',
+  integrations: [tailwind()],
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
   },
 });
-
